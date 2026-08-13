c9151a5c93c31182c12292c967a58411babf9332
Zelia-sss <3359719300@qq.com>
2026-08-13 15:57:00 +0800

    自制二维码

---
+++ QR_Code.py
@@ -0,0 +1,6 @@
+from MyQR import myqr
+myqr.run(words="photo",
+         save_name="photo.gif",
+         picture=r"C:\Users\azeaz\Pictures\Camera Roll\20251222143618_54_2.jpg",
+         colorized=True,
+         version=10)
